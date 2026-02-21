240f6b888a61b3bad113e5f7e62bbaa3b787e247
Gigil James <70816318+gigiljames@users.noreply.github.com>
2026-02-21 14:30:50 +0530

    adds health route

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import { ROUTES } from '../constants/routes';
 import { login, logout, refresh, sendOtp, signup } from '../controllers/userController';
+import { HTTP_STATUS_CODES } from '../constants/httpStatusCodes';
 
 const userRouter = Router();
 
@@ -10,6 +11,10 @@ userRouter.post(ROUTES.AUTH.LOGIN, (req: Request, res: Response, next: NextFunct
 
 userRouter.post(ROUTES.AUTH.LOGOUT, (req: Request, res: Response, next: NextFunction) => {
   logout(req, res, next);
+});
+
+userRouter.get(ROUTES.AUTH.ME, (req: Request, res: Response, next: NextFunction) => {
+  res.status(HTTP_STATUS_CODES.OK).json({ success: true, message: 'Hello. I am OK' });
 });
 
 userRouter.post(ROUTES.AUTH.SEND_OTP, (req: Request, res: Response, next: NextFunction) => {
